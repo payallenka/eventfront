84dbea3b650f011b6199d4b2ece172d797f5f15f
Payallenka <payalm.lenka@gmail.com>
2025-10-07 19:43:04 +0530

    creds

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://kaatweultfdcjvfzhwpm.supabase.co' 
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImthYXR3ZXVsdGZkY2p2Znpod3BtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk2NDM2MTUsImV4cCI6MjA3NTIxOTYxNX0.PG_7t-Zu2vdWK7dG6N3MBIGVNY986FHTiB9iJNK_H1g'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
